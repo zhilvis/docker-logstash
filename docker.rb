d24b288d3fb630b55b9029399d519353f89bffda
Zilvinas Kybartas <zilvinas@inventi.lt>
2014-10-12 15:16:47 +0300

    add cache

--- docker.rb
+++ docker.rb
@@ -1,27 +1,40 @@
-require 'json'
-require 'excon'
+require "logstash/filters/base"
 
 class LogStash::Filters::Docker < LogStash::Filters::Base
  config_name "docker"
- milestone 2
+ milestone 1
+
+ public
+ def initialize(config = {})
+  super
+  @cache = Hash.new
+ end
 
  public
  def register
-        # Nothing to do
+  require 'json'
+  require 'excon'
  end
 
  public
  def filter(event)
-  con = Excon.new("unix:///", {:socket => "/var/run/docker.sock"})
-  containers = JSON.load(con.get(:path => "/v1.14/containers/json?all=1").body)
+  return unless filter?(event)
+
   id = event["file"].split('/').last(2).first
-  container = containers.index{|b| b["Id"] == id}
-  if container
-    image_name = containers.at(container)["Names"].at(0)
-    event["container"] = image_name
+  if @cache[id]
+    image_name = @cache[id]
   else
-    event["container"] = "unknown"
+    con = Excon.new("unix:///", {:socket => "/var/run/docker.sock"})
+    containers = JSON.load(con.get(:path => "/v1.14/containers/json?all=1").body)
+    container = containers.index{|b| b["Id"] == id}
+    if container
+       image_name = containers.at(container)["Names"].at(0)
+    else
+       image_name = "unknwon"
+    end
+    @cache[id] = image_name
   end
+  event["container"] = image_name
   filter_matched(event)
  end
 end
